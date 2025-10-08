ed08bd9e1194e36da2df809ca1115eb9aa23fe2c
Juanppmi <116412973+Juanppmi@users.noreply.github.com>
2025-10-07 22:02:48 -0300

    Criacao da Api, implementacao do swagger e alteracao de url local

---
+++ Controllers/NotaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpModule.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotaController : Controller
+    {
+        public class Venda
+        {
+            public int Id { get; set; }
+            public string Cliente { get; set; }
+            public decimal Valor { get; set; }
+        }
+
+        [HttpPost]
+        public IActionResult GerarNota([FromBody] Venda venda)
+        {
+            string xml = $"<nota><id>{venda.Id}</id><cliente>{venda.Cliente}</cliente><valor>{venda.Valor}</valor></nota>";
+
+            return Ok(new { sucesso = true, xml });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,12 +6,18 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();   // <- Isso é importante para Swagger
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();                          // <- Gera documentação Swagger
+    app.UseSwaggerUI();
 }
 
 app.UseAuthorization();
