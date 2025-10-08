ff8515ece5a920a43a70ce7fbb192ff452911190
Juanppmi <116412973+Juanppmi@users.noreply.github.com>
2025-10-07 23:11:06 -0300

    Funcao do tef simulada

---
+++ Controllers/TefController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpModule.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TefController : Controller
+    {
+        public class Pagamento
+        {
+            public int IdVenda { get; set; }
+            public string Cartao { get; set; }
+            public decimal Valor { get; set; }
+        }
+
+        [HttpPost]
+        public IActionResult ProcessarPagamento([FromBody] Pagamento pagamento)
+        {
+            // Simula aprovação do pagamento
+            var aprovado = true;
+            var codigoAutorizacao = aprovado ? "123456" : null;
+
+            return Ok(new
+            {
+                sucesso = aprovado,
+                codigoAutorizacao,
+                mensagem = aprovado ? "Pagamento aprovado" : "Pagamento recusado"
+            });
+        }
+    }
+}
